da9f00e6159a23ee6a6afff97cc48028cbaa2cf6
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:32:37 -0500

    Updated component to version 1.9.1

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-flag',
+  summary : 'Semantic UI - Flag (official): Single component release of flag',
+  version : '1.9.1',
+  git     : 'git://github.com/Semantic-Org/UI-Flag.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
