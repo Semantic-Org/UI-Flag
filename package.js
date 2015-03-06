c49aab2db2a2b9b9976c73b631c662fdfd2e91de
Pusher Robot <jack@semantic-ui.com>
2015-03-06 18:21:52 -0500

    Updated component to version 1.11.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-flag',
   summary : 'Semantic UI - Flag: Single component release',
-  version : '1.11.1',
+  version : '1.11.2',
   git     : 'git://github.com/Semantic-Org/UI-Flag.git',
 });
 
