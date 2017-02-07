e36b23364b2aa6426e33fde725341ea6f57eff38
Jack Lukic <jack@semantic-ui.com>
2017-02-07 10:56:40 -0500

    Updated component to version 2.2.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-flag',
   summary : 'Semantic UI - Flag: Single component release',
-  version : '2.2.6',
+  version : '2.2.7',
   git     : 'git://github.com/Semantic-Org/UI-Flag.git',
 });
 
